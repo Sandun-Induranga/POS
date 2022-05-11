cb54bcd68dffc2d9040c8b248f686a14f51608ad
Sandun-Induranga <sandutharu40@gmail.com>
2022-05-11 20:51:50 +0530

    Create Class CustomDTO

--- Layered_Architecture/src/dao/custom/QueryDAO.java
+++ Layered_Architecture/src/dao/custom/QueryDAO.java
@@ -1,7 +1,10 @@
 package dao.custom;
 
+import model.CustomDTO;
+
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public interface QueryDAO {
-    public void searchOrderByOrderId(String id) throws SQLException, ClassNotFoundException;
+    public ArrayList<CustomDTO> searchOrderByOrderId(String id) throws SQLException, ClassNotFoundException;
 }
